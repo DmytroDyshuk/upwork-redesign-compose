878cd8099480176734f1bdb75ee9e3dc4022b226
Dmytro Dyshuk <dd.kronos@gmail.com>
2024-09-10 23:08:04 +0300

    Typography updated

---
+++ app/src/main/java/com/dyshuk/android/upworkredesigncompose/ui/theme/FontFamily.kt
@@ -0,0 +1,14 @@
+package com.dyshuk.android.upworkredesigncompose.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.dyshuk.android.upworkredesigncompose.R
+
+val rubikFamily = FontFamily(
+    Font(R.font.rubik_regular, FontWeight.Normal),
+    Font(R.font.rubik_light, FontWeight.Light),
+    Font(R.font.rubik_medium, FontWeight.Medium),
+    Font(R.font.rubik_semi_bold, FontWeight.SemiBold),
+    Font(R.font.rubik_bold, FontWeight.Bold)
+)
