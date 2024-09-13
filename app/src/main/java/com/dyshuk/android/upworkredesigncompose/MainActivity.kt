4091d51b4e06845854f03443adff65d78f1d866a
Dmytro Dyshuk <dd.kronos@gmail.com>
2024-09-13 15:53:54 +0300

    app launcher icon updated, added splash screen

--- app/src/main/java/com/dyshuk/android/upworkredesigncompose/MainActivity.kt
+++ app/src/main/java/com/dyshuk/android/upworkredesigncompose/MainActivity.kt
@@ -9,12 +9,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.dyshuk.android.upworkredesigncompose.ui.theme.UpworkRedesignComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        installSplashScreen()
         setContent {
             UpworkRedesignComposeTheme {
                 Box(modifier = Modifier.fillMaxSize()) {
