fc4f5fd60bd53deb2660c95cc0fde1f3b34bcc8e
Dmytro Dyshuk <dd.kronos@gmail.com>
2024-09-13 10:50:18 +0300

    Shapes updated

---
+++ app/src/main/java/com/dyshuk/android/upworkredesigncompose/ui/theme/Shapes.kt
@@ -0,0 +1,10 @@
+package com.dyshuk.android.upworkredesigncompose.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val StandardUpworkShape = Shapes(
+    small = RoundedCornerShape(10.dp),
+    medium = RoundedCornerShape(15.dp)
+)
--- app/src/main/java/com/dyshuk/android/upworkredesigncompose/ui/theme/Theme.kt
+++ app/src/main/java/com/dyshuk/android/upworkredesigncompose/ui/theme/Theme.kt
@@ -19,6 +19,7 @@ fun UpworkRedesignComposeTheme(content: @Composable () -> Unit) {
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
+        shapes = StandardUpworkShape,
         content = content
     )
 }
