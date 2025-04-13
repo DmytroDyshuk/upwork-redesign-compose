7c48ffc70fd6412765315d0b04055858624a7bc1
Dmytro Dyshuk <dd.kronos@gmail.com>
2025-04-13 15:06:53 +0300

    LoadingScreen ui update

--- app/src/main/java/com/dyshuk/android/upworkredesigncompose/ui/components/LoadingScreen.kt
+++ app/src/main/java/com/dyshuk/android/upworkredesigncompose/ui/components/LoadingScreen.kt
@@ -1,11 +1,19 @@
 package com.dyshuk.android.upworkredesigncompose.ui.components
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
-fun LoadingScreen() {
-    CircularProgressIndicator(modifier = Modifier.fillMaxSize())
+fun LoadingScreen(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator()
+    }
+
 }
