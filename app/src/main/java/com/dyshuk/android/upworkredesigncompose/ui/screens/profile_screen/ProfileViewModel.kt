d61961ca34ce05685c85bf22bfc77924dc96eb11
Dmytro Dyshuk <dd.kronos@gmail.com>
2025-06-09 16:19:57 +0300

    feat(ProfileScreen): add Feedback model, fake data and repository, update ProfileViewModel

--- app/src/main/java/com/dyshuk/android/upworkredesigncompose/ui/screens/profile_screen/ProfileViewModel.kt
+++ app/src/main/java/com/dyshuk/android/upworkredesigncompose/ui/screens/profile_screen/ProfileViewModel.kt
@@ -1,7 +1,33 @@
 package com.dyshuk.android.upworkredesigncompose.ui.screens.profile_screen
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.dyshuk.android.upworkredesigncompose.data.repository.FakeFeedbackRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 class ProfileViewModel(): ViewModel() {
+
+    private val _uiState = MutableStateFlow(ProfileScreenState())
+    val uiState: StateFlow<ProfileScreenState> = _uiState.asStateFlow()
+
+    init {
+        fetchFeedbackList()
+    }
+
+    private fun fetchFeedbackList() {
+        viewModelScope.launch {
+            _uiState.update { it.copy(feedbackListState = FeedbackListState.Loading) }
+            try {
+                val feedbackList = FakeFeedbackRepository.getFeedbackList()
+                _uiState.update { it.copy(feedbackListState = FeedbackListState.Success(feedbackList)) }
+            } catch (e: Exception) {
+                _uiState.update { it.copy(feedbackListState = FeedbackListState.Error("Loading Error: ${e.message}")) }
+            }
+        }
+    }
 
 }
