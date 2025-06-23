b08aa92330084b3501b92ca6ca8ac15a2cf50aa5
Dmytro Dyshuk <dd.kronos@gmail.com>
2025-06-23 15:12:04 +0300

    refactor(StarRating): change rating display logic

--- app/src/main/java/com/dyshuk/android/upworkredesigncompose/ui/components/indicators/StarRating.kt
+++ app/src/main/java/com/dyshuk/android/upworkredesigncompose/ui/components/indicators/StarRating.kt
@@ -1,17 +1,23 @@
 package com.dyshuk.android.upworkredesigncompose.ui.components.indicators
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.drawscope.clipRect
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.dyshuk.android.upworkredesigncompose.R
@@ -22,25 +28,45 @@ import com.dyshuk.android.upworkredesigncompose.ui.theme.rubikFamily
 @Composable
 fun StarRating(
     modifier: Modifier = Modifier,
-    rating: Int,
+    rating: Float,
     maxRating: Int = 5,
+    starSize: Dp = 11.dp,
     showTextRating: Boolean = false
 ) {
     Row(
         modifier = modifier
     ) {
         for (i in 1..maxRating) {
-            val starColor = if (i <= rating) {
-                Color(0xFFFFD700)
-            } else {
-                Color(0xFFD3D3D3)
+            val fillFraction = rating - (i - 1)
+
+            Box(modifier = Modifier.size(starSize)) {
+                Image(
+                    modifier = Modifier
+                        .matchParentSize()
+                        .clip(RectangleShape),
+                    painter = painterResource(id = R.drawable.ic_vote_star),
+                    contentDescription = null,
+                )
+                Image(
+                    modifier = Modifier
+                        .matchParentSize()
+                        .clip(RectangleShape)
+                        .drawWithContent {
+                            val clipWidth = size.width * fillFraction
+                            clipRect(
+                                left = 0f,
+                                top = 0f,
+                                bottom = size.height,
+                                right = clipWidth
+                            ) {
+                                this@drawWithContent.drawContent()
+                            }
+                        },
+                    painter = painterResource(id = R.drawable.ic_vote_star),
+                    colorFilter = ColorFilter.tint(color = Color(0xFFFFBA49)),
+                    contentDescription = null
+                )
             }
-            Image(
-                painter = painterResource(id = R.drawable.ic_vote_star),
-                contentDescription = null,
-                modifier = Modifier.size(11.dp),
-                colorFilter = ColorFilter.tint(starColor)
-            )
         }
 
         if (showTextRating) {
@@ -60,6 +86,6 @@ fun StarRating(
 @Composable
 fun StarRatingPreview() {
     UpworkRedesignComposeTheme {
-        StarRating(rating = 3, showTextRating = true)
+        StarRating(rating = 4.5f, showTextRating = true)
     }
 }
